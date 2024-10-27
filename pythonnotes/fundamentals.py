48579012047b068ca78dbbf1949f1e1b922f59a0
naveenkumar1117 <workalpha007@gmail.com>
2024-10-27 13:51:10 +0530

    Add New Files

---
+++ pythonnotes/fundamentals.py
@@ -0,0 +1,43 @@
+# variables and literals in Python.
+# Python Variables
+# In programming, a variable is a container (storage area) to hold data. For example,
+
+number = 10
+# Here, number is a variable storing the value 10.
+print(number)
+
+# As we can see from the above example, we use the assignment operator = to assign a value to a variable.
+# assign value to site_name variable
+site_name = 'learn_python'
+
+print(site_name)
+# Output: learn_python
+
+'''Python Numeric Literals
+Numeric Literals are immutable (unchangeable). 
+Numeric literals can belong to 3 different numerical types: 
+Integer, Float, and Complex.
+
+1. Integer Literals
+Integer literals are numbers without decimal parts. 
+It also consists of negative numbers. For example, 5, -11, 0, 12, etc.
+
+2. Floating-Point Literals
+Floating-point literals are numbers that contain decimal parts.
+
+Just like integers, floating-point numbers can also be both positive and negative. 
+For example, 2.5, 6.76, 0.0, -9.45, etc.
+
+3. Complex Literals
+Complex literals are numbers that represent complex numbers.
+Here, numerals are in the form a + bj, where a is real and b is imaginary. 
+For example, 6+9j, 2+3j.'''
+
+#Python String Literals
+'''In Python, texts wrapped inside quotation marks are called string literals..
+
+"This is a string."
+We can also use single quotes to create strings.
+
+'This is also a string.'
+'''
---
+++ pythonnotes/introduction.py
@@ -0,0 +1,28 @@
+# --------- Learn Python ---------
+
+'''Python is a versatile, high-level programming language that
+   is widely supported across all major operating systems.
+   Now, let's write a simple Python program.
+   The following program displays Hello, World! on the screen.'''
+
+print("hello world")
+
+'''In Python, anything inside print() is displayed on the screen.
+   There are two things to note about print():
+   1. Everything we want to display on the screen is included inside the parentheses ().
+   2. The text we want to print is placed within double quotes " ", single quotes ' ' can also be used. 
+'''
+# --------- Comments ---------
+# Now, let's learn about Python comments.
+# Comments are hints that we add to our code to make it easier to understand.
+# Python comments start with #. For example,
+
+# This is a single line comment
+# print a number
+print(25)
+
+# This is a Multiline Comments
+'''Multiline Comments
+   Multiline Comments
+   Multiline Comments
+'''
